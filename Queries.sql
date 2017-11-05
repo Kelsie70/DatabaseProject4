bbbf7b16cd66dc78a2a198da0f95ae7e76c768e1
Kyle Millar <kymilla608@gmail.com>
2017-11-05 17:05:43 -0500

    Added queries 5 and 6

--- Queries.sql
+++ Queries.sql
@@ -16,4 +16,26 @@ AND t.crsCode = h.crsCode
 AND h.profId = p.id
 AND p.name = 'name296707';
 
+-- Query 5
+SELECT S.name
+FROM Student S, Transcript T, Course C
+WHERE S.id = T.studId
+	T.crsCode = C.crsCode
+	C.deptID = v6
 
+MINUS
+
+SELECT S.name
+FROM Student S, Transcript T, Course C
+WHERE S.id = T.studID
+	T.crsCode = C.crsCode
+	C.deptID = v7
+
+--Query 6
+SELECT S.name
+FROM Student S, Transcript T 
+WHERE S.id = T.studID
+	T.crsCode IN
+	(SELECT T.crsCode FROM Course C WHERE C.deptID = v8)
+GROUP BY T.studID
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Course C WHERE C.deptID = v8)
