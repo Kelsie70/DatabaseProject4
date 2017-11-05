057fed28eff04be65276ce6db25f6db178ebaeae
Kelsie Belan <kelsie70@gmail.com>
2017-11-05 17:01:56 -0500

    4th query

--- indexedSQLQueries.sql
+++ indexedSQLQueries.sql
@@ -10,6 +10,9 @@ ON db_project_4.transcript;
 DROP INDEX crs_code
 ON db_project_4.transcript;
 
+DROP INDEX teaching_crs_code
+on db_project_4.teaching;
+
 CREATE INDEX prof_id
 ON db_project_4.professors (id);
 
@@ -21,6 +24,9 @@ ON db_project_4.transcript (id);
 
 CREATE INDEX crs_code
 ON db_project_4.transcript (crsCode);
+
+CREATE INDEX teaching_crs_code
+ON db_project_4.teaching (crsCode);
 
 SELECT name
 FROM db_project_4.professors
@@ -35,9 +41,23 @@ WHERE id > 10000 AND id < 15000;
 SELECT name
 FROM db_project_4.students
 USE INDEX(stud_id)
-JOIN db_project_4.transcript 
-ON db_project_4.transcript.id = db_project_4.students.id
+	JOIN db_project_4.transcript 
+	USE INDEX(trans_stud_id, crs_code)
+		ON db_project_4.transcript.id = db_project_4.students.id
 WHERE db_project_4.transcript.crsCode = 'crsCode394193';
 
+SELECT name
+FROM db_project_4.students
+USE INDEX(stud_id)
+	JOIN db_project_4.transcript
+	USE INDEX(trans_stud_id)
+		ON db_project_4.transcript.id = db_project_4.students.id
+	JOIN db_project_4.teaching
+    USE INDEX(teaching_crs_code)
+		ON db_project_4.teaching.crsCode = db_project_4.transcript.crsCode 
+			AND db_project_4.teaching.semester = db_project_4.transcript.semester
+	JOIN db_project_4.professors
+		ON db_project_4.professors.id = db_project_4.teaching.id
+WHERE db_project_4.professors.name = 'name296707';
 
 
