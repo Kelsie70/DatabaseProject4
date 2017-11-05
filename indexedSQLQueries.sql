a04bf3a48ad20e8f56b6269a7f8048ac403d07ba
Kelsie Belan <kelsie70@gmail.com>
2017-11-05 16:40:15 -0500

    added queries 1-3

--- indexedSQLQueries.sql
+++ indexedSQLQueries.sql
@@ -1,15 +1,43 @@
-CREATE INDEX prof_id
-ON db_project_4.Professor (id);
+DROP INDEX prof_id
+ON db_project_4.professors;
 
-SELECT * 
-FROM db_project_4.Professor
+DROP INDEX stud_id
+ON db_project_4.students;
+
+DROP INDEX trans_stud_id
+ON db_project_4.transcript;
+
+DROP INDEX crs_code
+ON db_project_4.transcript;
+
+CREATE INDEX prof_id
+ON db_project_4.professors (id);
+
+CREATE INDEX stud_id
+ON db_project_4.students (id);
+
+CREATE INDEX trans_stud_id
+ON db_project_4.transcript (id);
+
+CREATE INDEX crs_code
+ON db_project_4.transcript (crsCode);
+
+SELECT name
+FROM db_project_4.professors
 USE INDEX(prof_id)
 WHERE id = 7522;
 
-SELECT *
-FROM db_project_4.Professor
+SELECT name
+FROM db_project_4.professors
 USE INDEX(prof_id)
 WHERE id > 10000 AND id < 15000; 
 
-DROP INDEX prof_id
-ON db_project_4.Professor;
+SELECT name
+FROM db_project_4.students
+USE INDEX(stud_id)
+JOIN db_project_4.transcript 
+ON db_project_4.transcript.id = db_project_4.students.id
+WHERE db_project_4.transcript.crsCode = 'crsCode394193';
+
+
+
