a43dd30afa2319d816842a850cfbb3cddc5aa525
Kyle Millar <kymilla608@gmail.com>
2017-11-06 23:33:18 -0500

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,15 +1,15 @@
 
 -- Query 1
-SELECT name FROM dbms4.students WHERE id = 5101;
+SELECT name FROM Students WHERE id = 5101;
 
 -- Query 2
-SELECT name FROM dbms4.students WHERE id < 15000 AND id > 10000;
+SELECT name FROM Students WHERE id < 15000 AND id > 10000;
 
 -- Query 3
-SELECT name FROM students s, transcript t WHERE s.id = t.studId AND t.crsCode = 'crsCode394193';
+SELECT name FROM Students s, Transcript t WHERE s.id = t.studId AND t.crsCode = 'crsCode394193';
 
 -- Query 4
-SELECT s.name FROM students s, transcript t, teaching h, professors p WHERE 
+SELECT s.name FROM Students s, Transcript t, Teaching h, Professor p WHERE 
 s.id = t.studId
 AND t.semester = h.semester
 AND t.crsCode = h.crsCode
@@ -18,24 +18,24 @@ AND p.name = 'name296707';
 
 -- Query 5
 SELECT S.name
-FROM Student S, Transcript T, Course C
+FROM Student S, Transcript T, Courses C
 WHERE S.id = T.studId
 	T.crsCode = C.crsCode
-	C.deptID = v6
+	C.deptID = 'deptId859888'
 
 MINUS
 
 SELECT S.name
-FROM Student S, Transcript T, Course C
+FROM Student S, Transcript T, Courses C
 WHERE S.id = T.studID
 	T.crsCode = C.crsCode
-	C.deptID = v7
+	C.deptID = 'deptId254977';
 
 --Query 6
 SELECT S.name
 FROM Student S, Transcript T 
 WHERE S.id = T.studID
 	T.crsCode IN
-	(SELECT T.crsCode FROM Course C WHERE C.deptID = v8)
+	(SELECT T.crsCode FROM Course C WHERE C.deptID = 'deptId859888')
 GROUP BY T.studID
-HAVING COUNT(*) = (SELECT COUNT(*) FROM Course C WHERE C.deptID = v8)
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Course C WHERE C.deptID = 'deptId859888');
