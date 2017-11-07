358adeafed92ca012b3b066693658888f901917f
Kelsie Belan <kelsie70@gmail.com>
2017-11-07 15:49:28 -0500

    changed 1st and 2nd queries to students

--- indexedSQLQueries.sql
+++ indexedSQLQueries.sql
@@ -14,6 +14,11 @@ ON db_project_4.transcript;
 DROP INDEX teaching_crs_code
 on db_project_4.teaching;
 
+DROP INDEX courses_crs_code
+on db_project_4.courses;
+
+DROP INDEX courses_dept_id
+on db_project_4.courses;
 
 CREATE INDEX prof_id
 ON db_project_4.professors (id);
@@ -30,14 +35,20 @@ ON db_project_4.transcript (crsCode);
 CREATE INDEX teaching_crs_code
 ON db_project_4.teaching (crsCode);
 
-SELECT name
-FROM db_project_4.professors
-USE INDEX(prof_id)
-WHERE id = 7522;
+CREATE INDEX courses_crs_code
+ON db_project_4.courses (crsCode);
+
+CREATE INDEX courses_dept_id
+ON db_project_4.courses (deptId);
 
 SELECT name
-FROM db_project_4.professors
-USE INDEX(prof_id)
+FROM db_project_4.students
+USE INDEX(stud_id)
+WHERE id = 12345;
+
+SELECT name
+FROM db_project_4.students
+USE INDEX(stud_id)
 WHERE id > 10000 AND id < 15000; 
 
 SELECT name
@@ -65,27 +76,33 @@ WHERE db_project_4.professors.name = 'name296707';
 
 SELECT db_project_4.students.name
 FROM db_project_4.students USE INDEX (stud_id), 
-	db_project_4.transcript USE INDEX(trans_stud_id, crs_code), db_project_4.courses
+	db_project_4.transcript USE INDEX(trans_stud_id, crs_code), 
+    db_project_4.courses USE INDEX(courses_crs_code)
 WHERE db_project_4.students.id = db_project_4.transcript.id 
 	AND db_project_4.transcript.crsCode = db_project_4.courses.crsCode
     AND db_project_4.courses.deptId = 123
 	AND NOT EXISTS (
 		SELECT db_project_4.students.name
-		FROM db_project_4.students, db_project_4.transcript, db_project_4.courses
+		FROM db_project_4.students USE INDEX (stud_id), 
+			db_project_4.transcript USE INDEX(trans_stud_id, crs_code), 
+            db_project_4.courses USE INDEX(courses_crs_code)
 		WHERE db_project_4.students.id = db_project_4.transcript.id
 			AND db_project_4.transcript.crsCode = db_project_4.courses.crsCode
 			AND db_project_4.courses.deptId = 456);
             
 
 SELECT db_project_4.students.name
-FROM db_project_4.students
-JOIN db_project_4.transcript ON db_project_4.students.id = db_project_4.transcript.id
+FROM db_project_4.students USE INDEX (stud_id)
+JOIN db_project_4.transcript USE INDEX (trans_stud_id)
+	ON db_project_4.students.id = db_project_4.transcript.id
 WHERE crsCode IN
 	(SELECT crsCode 
-     FROM db_project_4.courses 
+     FROM db_project_4.courses USE INDEX (courses_crs_code, courses_dept_id)
 	 WHERE deptId = 12345)
 GROUP BY db_project_4.transcript.id
 HAVING COUNT(*) = (SELECT COUNT(*) 
-					FROM db_project_4.courses 
+					FROM db_project_4.courses USE INDEX (courses_dept_id)
 					WHERE db_project_4.courses.deptId = '12345');
+
+
 
